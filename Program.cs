fec6cf22aced2b4d9ad3a1c530015d5c748f8cb1
HyperKwonHyukmin <khm7529@gmail.com>
2026-03-13 16:39:32 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
 using ModuleGroupUnitAnalysis.Logger;
-using ModuleGroupUnitAnalysis.Pipeline; // 파이프라인 네임스페이스 추가
+using ModuleGroupUnitAnalysis.Pipeline;
 
 namespace ModuleGroupUnitAnalysis
 {
+  // 1. 해석 타입을 정의하는 Enum 추가
+  public enum AnalysisType
+  {
+    ModuleUnit,
+    GroupUnit
+  }
+
   class MainApp
   {
     static void Main(string[] args)
     {
-      // 강체역학망 오류 케이스 
-      //var bdfFile = @"C:\Coding\Csharp\Projects\ModuleGroupUnitAnalysis\KangSangHunCSV\MUnit\3515_35020\NastranTest\3515-35020.bdf";
       var bdfFile = @"C:\Coding\Csharp\Projects\ModuleGroupUnitAnalysis\KangSangHunCSV\MUnit\3515_35030\NastranTest\3515-35030.bdf";
 
       // 1. 디버그 및 로그 옵션 설정
@@ -16,22 +21,28 @@ namespace ModuleGroupUnitAnalysis
       bool pipelineDebug = true;
       bool verboseDebug = false;
 
-      // Sanity 체크용 Nastran을 실제로 구동할 것인가? (시간 절약을 위해 평소엔 false)
+      // =========================================================
+      // [파이프라인 제어 스위치 모음]
+      // =========================================================
+      // ★ 2. 여기서 해석 타입을 결정합니다.
+      AnalysisType analysisType = AnalysisType.ModuleUnit;
+
       bool runSanityNastranCheck = false;
 
-      // Ubolt DOF 강제 "123456" 설정
-      bool forceRigidDof123456 = true;
+      // Group Unit일 때는 무조건 123456을 적용하도록 연동
+      bool forceRigidDof123456 = (analysisType == AnalysisType.GroupUnit);
 
-      // Nastran 해석 유무
       bool runNastranAnalysis = true;
+      bool checkAnalysisResult = true;
+      // =========================================================
 
       // 2. 로거 초기화 및 로그 파일 생성 경로 확정
       var logger = new PipelineLogger(logExport);
       logger.InitializeFile(bdfFile);
 
-      // 3. 파이프라인 객체 생성 및 실행 
-      var pipeline = new HookTrolleyPipeline(bdfFile, logger, runSanityNastranCheck, 
-        forceRigidDof123456, runNastranAnalysis, pipelineDebug, verboseDebug);
+      // 3. 파이프라인 객체 생성 및 실행 (옵션 추가)
+      var pipeline = new HookTrolleyPipeline(bdfFile, logger, analysisType, runSanityNastranCheck,
+              forceRigidDof123456, runNastranAnalysis, checkAnalysisResult, pipelineDebug, verboseDebug);
       pipeline.Run();
     }
   }
